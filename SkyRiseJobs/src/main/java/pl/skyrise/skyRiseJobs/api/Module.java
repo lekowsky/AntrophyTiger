0d5dccf0c7c70de9df1080107038fde890b8c76f
lekowsky <olaf.zrobek@gmail.com>
2026-06-26 19:02:42 +0200

    Delete SkyRiseJobs directory

--- SkyRiseJobs/src/main/java/pl/skyrise/skyRiseJobs/api/Module.java
+++
@@ -1,8 +0,0 @@
-package pl.skyrise.skyRiseJobs.api;
-
-public interface Module {
-    String getName();
-    void onEnable();
-    void onDisable();
-    default void onReload() {}
-}
--- SkyRiseJobs/src/main/java/pl/skyrise/skyRiseJobs/core/level/LevelAddResult.java
+++
@@ -1,3 +0,0 @@
-package pl.skyrise.skyRiseJobs.core.level;
-
-public record LevelAddResult(double addedExp, int oldLevel, int newLevel, boolean leveledUp) {}
